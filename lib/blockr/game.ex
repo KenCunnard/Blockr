91471e8a1d5401519da9f39b04c6fdec0b07fd68
Ken <openproxy@gmail.com>
2025-07-15 22:58:06 -0400

    My implementation of Checkpoint 13

--- lib/blockr/game.ex
+++ lib/blockr/game.ex
@@ -1,4 +1,5 @@
 defmodule Blockr.Game do
+  alias Blockr.Game.Shape
   alias Blockr.Game.Tetromino
 
   def left(board) do
@@ -19,20 +20,37 @@ defmodule Blockr.Game do
     attempt_move(board, updated_tetro)
   end
 
+  def fall(board) do
+    updated_tetro = Tetromino.fall(board.tetro)
+
+    attempt_move(board, updated_tetro)
+  end
+
   defp attempt_move(board, tetro) do
     set =
       tetro
       |> Tetromino.to_group()
       |> MapSet.new()
 
-    intersection = MapSet.intersection(set, board.points)
+    junk = Enum.map(board.junkyard, fn {point, _color} -> point end)
+    points = board.points |> MapSet.to_list()
+
+    updated_points =
+      [junk | points]
+      |> List.flatten()
+      |> MapSet.new()
+
+    updated_junkyard =
+      [tetro |> Tetromino.to_group() |> Shape.paint(tetro.name) | board.junkyard]
+
+    intersection = MapSet.intersection(set, updated_points)
 
     should_move? = MapSet.size(intersection) == 0
 
     if should_move? do
       %{board | tetro: tetro}
     else
-      board
+      %{board | tetro: nil, points: updated_points, junkyard: updated_junkyard}
     end
   end
 end
